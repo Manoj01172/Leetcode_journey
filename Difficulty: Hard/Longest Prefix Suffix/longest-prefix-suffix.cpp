29a5b7cd2baa7ade5809e72dc034dfc97cecb90c
Manoj01172 <m9901172@gmail.com>
2026-08-07 18:23:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int getLPSLength(string &s) {
+        // code here
+        vector<int> lps(s.size(),0);
+        int prefix =0 ;
+        int suffix =1;
+        while(suffix<s.size()){
+            if(s[suffix]==s[prefix]){
+                lps[suffix]=prefix+1;
+                prefix++;
+                suffix++;
+            }
+            else{
+                if(prefix==0){
+                    lps[suffix]=0;
+                    suffix++;
+                }
+                else{
+                    prefix=lps[prefix-1];
+                }
+            }
+        }
+        return lps[s.size()-1];
+    }
+};
